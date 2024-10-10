b9911b10779045640cfae65d14cc687d97d22f82
zett <quangnn906@gmail.com>
2024-10-10 18:37:47 +0700

    Create Base Project

---
+++ src/test/java/com/zett/recipeapi/RecipeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zett.recipeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
