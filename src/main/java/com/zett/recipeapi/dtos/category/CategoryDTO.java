2dac8d098754b1d705f7c5fdc2f3979836931c55
zett <quangnn906@gmail.com>
2024-10-10 18:38:52 +0700

    Create Category entity

---
+++ src/main/java/com/zett/recipeapi/dtos/category/CategoryDTO.java
@@ -0,0 +1,23 @@
+package com.zett.recipeapi.dtos.category;
+
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO {
+    private UUID id;
+
+    @NotNull(message = "Name is required")
+    @Length(min = 3, max = 100, message =  "Name must be between 3 and 100 characters")
+    private String name;
+    
+    @Length(max = 1000, message = "Description must be less than 1000 characters")
+    private String description;
+}
